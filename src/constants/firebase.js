eba3717a53e445c37f41a6b838ea4629c5ae8d14
Damian Buttle <damianbuttle@gmail.com>
2018-06-05 11:08:00 -0500

    initial commit

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyB9jWgXPXAiT3YHNeLhDDno7_8dTYMbFL0',
+  authDomain: 'motoron-11f75.firebaseapp.com',
+  databaseURL: 'https://motoron-11f75.firebaseio.com',
+  projectId: 'motoron-11f75',
+  storageBucket: 'motoron-11f75.appspot.com',
+  messagingSenderId: '610130602376',
+};
